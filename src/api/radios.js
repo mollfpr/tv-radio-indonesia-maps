3c6f5e7750dc35cea12b51b7cdecd6bfbd8f068f
luthfi <luthfi.ufi14@gmail.com>
2020-04-06 21:16:32 +0700

    U: api

--- src/api/radios.js
+++ src/api/radios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const radios = async province => {
   const { data } = await axios.get(
-    `http://blooming-forest-86922.herokuapp.com/api/radios/${province}`
+    `https://map.gampangngiklan.com/admin/api/radios/${province}`
   );
 
   return data;
--- src/api/televisions.js
+++ src/api/televisions.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const televisions = async province => {
   const { data } = await axios.get(
-    `http://blooming-forest-86922.herokuapp.com/api/televisions/${province}`
+    `https://map.gampangngiklan.com/admin/api/televisions/${province}`
   );
 
   return data;
